af6903a38d2ed53744059878f41472fdd7cfba0a
learningwithnicole <admin@learningwithnicoleltd.co.uk>
2025-07-22 23:44:16 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === 'production' ? '/learning-with-nicole-website' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/learning-with-nicole-website/' : '',
   trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
