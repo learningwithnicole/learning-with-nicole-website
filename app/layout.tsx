02080bfe59ae13f456d82074d061afc53a5de416
learningwithnicole <admin@learningwithnicoleltd.co.uk>
2025-08-09 00:23:40 +0100

    add google analytics script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Learning with Nicole | Online Diploma in Childcare',
@@ -13,6 +14,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {/* Google Analytics Global Site Tag (gtag.js) */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-R251H38D8J"
+        strategy="afterInteractive" // Loads after the page becomes interactive
+      />
+
+      {/* Google Analytics Initialization */}
+      <Script
+        id="google-analytics-init" // Unique ID for the inline script
+        strategy="afterInteractive" // Loads after the page becomes interactive
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-R251H38D8J');
+          `,
+        }}
+      />
       <body>{children}</body>
     </html>
   )
